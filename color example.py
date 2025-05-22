9fb856df72a46e355203c8eebc45c20ba0f954d3
Hasti2020 <hasti1086m@gmail.com>
2025-05-22 08:36:14 -0600

    Create color example.py

---
+++ color example.py
@@ -0,0 +1,12 @@
+# Color codes using ANSI escape sequences
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+ENDC = "\033[0m"  # Reset to default
+
+# Print examples
+print(RED + "This is red like Gryffindor." + ENDC)
+print(GREEN + "This is green like Slytherin." + ENDC)
+print(YELLOW + "This is yellow like Hufflepuff." + ENDC)
+print(BLUE + "This is blue like Ravenclaw." + ENDC)
