2fe36ee669330354b2d33c6d433b0074209bc766
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-05-26 16:40:16 -0600

    Update character.py

--- character.py
+++ character.py
@@ -14,40 +14,49 @@ class Character:
         self.health -= damage
         if self.health <= 0:
             self.alive = False
-            print(f"-Hagrid saying youre dead-")
+            print("lol u r dead")
         else: 
-            print(f"{self.name} took damage. Current health = {self.health}")
+            print(f"{self.name}, current health : {self.health}")
 
 class Player(Character):
     
-    def __init__(self, name, wand_type):
-        super.__init__(name)
-        self.wand_type = wand.Wand(min_power, max_power)
+    def __init__(self, name):
+        Character.__init__(self, name)
+        self.wand_type = wand.Wand.get_wand()
+
         
-    def inflict_damage(self, hero):
-        print("Casting a spell...")
-        attack = wand.cast_spell(self)
-        print(f"the enemy has taken {attack} damage")
+    def inflict_damage(self, enemy):
+        print("\nCasting a spell...")
+        attack = random.randint(self.wand_type.min_power, self.wand_type.max_power)
+        print(f"{oponent} has taken {attack} damage")
         enemy.take_damage(attack)
         
        
-class Oponent(Player):
+class Oponent(Character):
     
     def __init__(self, name):
-        super.__init__(name)
+        Character.__init__(self, name)
      
-    def inflict_damage(self, enemy):
-        if oponent == 'Luna':
+    def inflict_damage(self, user):
+        print(f"{oponent} is casting a spell...")
+        if self.name == 'Luna':
             attack = random.randint(0, 10)
-        elif oponent == 'Draco':
-            attack == random.randint(10, 20)
-        elif oponent == 'Peter':
+        if self.name == 'Draco':
+            attack = random.randint(5, 20)
+        if self.name == 'Peter':
+            attack = random.randint(15, 30)
+        if self.name == 'Bellatrix':
+            attack = random.randint(20, 35)
+        print(f"you have taken {attack} damage")
+        user.take_damage(attack)
+
         
     
  
-oponent_list = ['Draco', 'Luna', 'Peter', 'Bellatrix', 'Cedric']
-oponent = random.randint(oponent_list)
+oponent_list = ['Draco', 'Luna', 'Peter', 'Bellatrix']
+oponent = random.choice(oponent_list)
                 
+
 
 
 
