0053f1ae812a5c306df516135073138980eae560
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-05-26 16:40:19 -0600

    Update wand.py

--- wand.py
+++ wand.py
@@ -10,14 +10,15 @@ class Wand:
 
     def __str__(self):
         return f"{self.name}"
-    
+    '''
     def cast_spell(self):
         return random.randint(self.min_power, self.max_power)
-    
+    '''
     def get_wand():
         random_wand = random.choice(wand_options)
-        print(f"The wand has spoken... You have been chosen by {random_wand}!")
-        print(f"Your wand has a {random_wand.core};  and casts spells with power between {random_wand.min_power} and {random_wand.max_power}.")
+        #print(f"The wand has spoken... You have been chosen by {random_wand}!")
+       #print(f"Your wand has a {random_wand.core};  and casts spells with power between {random_wand.min_power} and {random_wand.max_power}.")
+        return random_wand
         
 wand_options = [Wand("Duststick", "Rabbit Fur", 0, 10),
                 Wand("Ashlight", "Unicorn Hair", 5, 15),
@@ -26,6 +27,6 @@ wand_options = [Wand("Duststick", "Rabbit Fur", 0, 10),
                 Wand("Elder Echo", "Basilisk Horn", 20, 30)]
 
 
-#Wand.get_wand()
+Wand.get_wand()
 
 
