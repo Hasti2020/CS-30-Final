e675c61fa99bd3061605a3bd99495d10d1158a05
Khanh Trinh <khanh.trinh1@rbe.sk.ca>
2025-05-27 09:32:18 -0600

    Update battle.py

--- battle.py
+++ battle.py
@@ -1,18 +1,38 @@
-import wand
 import character
+import random
+oponent_list = ['Draco', 'Luna', 'Peter', 'Bellatrix']
+oponent = random.choice(oponent_list) 
 player = character.Player('Harry')
-target = character.Oponent(character.oponent)
+target = character.Oponent(oponent)
+
 
 while player.alive and target.alive:
     player.inflict_damage(target)
     if target.alive:
         target.inflict_damage(player)
     choice = input('enter to continue: ')
-    
-if player.alive: 
-    print("\nCongrats you survived and won the battle")
+
+
+if not player.alive:
+    print("better luck next time kiddo")
 else:
-    print("\nBetter Luck next time.")
+    print("\nCongrats you survived and won the battle")
+    print(f"But oh no {player.name}! Lord Voldemort just appeared on the battlefield! Use all the stength you have left to battle him!")
+    oponent = 'Voldemort'
+    boss = character.Oponent('Voldemort')
+    while player.alive and boss.alive:
+        player.inflict_damage(boss)
+        if boss.alive:
+            boss.inflict_damage(player)
+    if player.alive:
+        print("you won")
+    else:
+        print("u lost")
+    choice = input('enter to continue: ')
+
+
+    
+
 
 
 
