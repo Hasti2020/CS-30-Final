1b41d54fca561f7b472e5ebd10fa43db65f761fb
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-05-28 08:47:44 -0600

    Update spell_lessons.py

--- spell_lessons.py
+++ spell_lessons.py
@@ -1,9 +1,10 @@
 
-spells = ['Alohomora',
+spells = ['Sectumsempra',
           'Lumos',
           'Avada Kedavra',
           'Disillusionment',
           'Cruciatus', 
+          'Expelliarmus',
 ]
 
 for item in spells:
@@ -20,8 +21,18 @@ class Spell:
         return f"{self.name}"
     
     def spell_power(self):
+        if self.name == 'Sectumsempra':
+            return 25
         if self.name == 'Lumos':
+            return 0
+        if self.name == 'Avada Kedavra':
+            return 20
+        if self.name == 'Disillusionment':
             return 5
+        if self.name == 'Cruciatus':
+            return 10
+        if self.name == 'Expelliarmus':
+            return 15
     
     def cast_spell(self):
         print(f"You have casted {self.name}, which has an attack damage of {self.attack}")
