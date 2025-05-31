d6f91793f277121c37e44a138a835f13e71e5735
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-05-30 23:23:55 -0600

    Update potion.py

--- potion.py
+++ potion.py
@@ -1,5 +1,6 @@
 import time
 
+
 class PotionGame:
     def __init__(self):
         self.ingredients = ['Horklump Juice', 'Dittany Leaves', 'Leech Juice', 'Spider Fang', 'Leech Juice', 'Stench of the Dead']
@@ -9,6 +10,7 @@ class PotionGame:
             'Thunderbrew Potion': ['Leech Juice', 'Stench of the Dead']
         }
         self.inventory = []
+        self.potion_inventory = []
 
     def things_on_table(self):
         global choice
@@ -24,34 +26,51 @@ class PotionGame:
                 self.inventory.append(selected)
                 self.ingredients.pop(choice-1)
                 return selected
+            
         except ValueError:
             print("numbers only")
             return None
-
+        
+    '''for testing purposes'''
     def view_inventory(self):
-        global choice
         i = 1
         for item in self.inventory:
             print(f"{i}. {item}")
             i += 1
     
     def brew_potion(self):
-        for potion, required_items in self.potions():
-            if all(item in self.inventory for item in required_items):
-                for item in required_items:
-                    self.inventory(item)
-                    print(f'you have brewed {potion}')
+        brew = False
+        print("brewing...")
+        time.sleep(1)
+        print("brewing...")
+        time.sleep(1)
+        print("almost done!")
+        time.sleep(1)
+        for potion, required_items in self.potions.items():
+                if all(item in self.inventory for item in required_items):
+                        for item in required_items:
+                            self.inventory.remove(item)
+                        print(f'you have brewed {potion}')
+                        self.potion_inventory.append(potion)
+                        brew = True
+        if not brew:
+            print("you do not have enough inredients to brew any potions!")
 
+    def potions_made(self):
+        return self.potion_inventory
  
-game = PotionGame()
-while True:        
-    test = input("q or w: ")
-    if test == 'q':
-        game.things_on_table() 
-    elif test == 'w':
-        game.my_inventory()
-    elif test == 'e':
-        game.brew_potion()
 
-      
+def start_potion_game():
+    game = PotionGame()
+    while True:     
+        test = input("q or w: ")
+        if test == 'q':
+            game.things_on_table() 
+        elif test == 'w':
+            game.view_inventory()
+        elif test == 'e':
+            game.brew_potion()
+        elif test == 'p':
+            print(game.potions_made())
+    return game.potions_made()  
 
