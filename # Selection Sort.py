af771dd7f263ed1328a0d795d9d64e74ad4591c5
Hasti2020 <hasti1086m@gmail.com>
2025-06-03 08:36:35 -0600

    Create # Selection Sort.py

---
+++ # Selection Sort.py
@@ -0,0 +1,10 @@
+# Selection Sort
+
+unsorted_list = [5, 2, 7, 3, 8, 1]
+
+def selection_sort(list_):
+    return list_
+
+sorted_list = selection_sort(unsorted_list.copy())
+print(unsorted_list)
+print(sorted_list)
