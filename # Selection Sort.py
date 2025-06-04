b791d470efc95e3a466b0842ef53e5e374b2236f
Hasti2020 <hasti1086m@gmail.com>
2025-06-04 08:34:49 -0600

    Update # Selection Sort.py

--- # Selection Sort.py
+++ # Selection Sort.py
@@ -3,8 +3,19 @@
 unsorted_list = [5, 2, 7, 3, 8, 1]
 
 def selection_sort(list_):
+    for i in range(len(list_) -1):
+        smallest_index = i
+        for j in range(i+1, len(list_)):
+            if list_[j] < list_[smallest_index]:
+                smallest_index = j
+        if smallest_index != i:
+            list_[smallest_index], list_[i] = list_[i], list_[smallest_index] #shortcut
+            #smallest = list_[smallest_index]
+            #list_[smallest_index] = list_[i]
+            #list_[i] = smallest
+        print("partial sort: ", list_)
     return list_
 
-sorted_list = selection_sort(unsorted_list.copy())
 print(unsorted_list)
+sorted_list = selection_sort(unsorted_list.copy())
 print(sorted_list)
