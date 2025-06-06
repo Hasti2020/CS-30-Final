f5aea5d673af9004935d704abfa9f8f96cb27c66
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-06-06 09:29:35 -0600

    Update test_main_game.py

--- test_main_game.py
+++ test_main_game.py
@@ -3,11 +3,11 @@ import battle
 
 
 def main():
-    potions = ['Thunderbrew Potion']
+
     while True:
         choice = input('a or s: ')
         if choice == 's':
             potions = potion.start_potion_game()
         elif choice == 'a':
-            battle.start_battle(potions)
+            battle.start_battle()
 main()
