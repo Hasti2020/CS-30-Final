bdf368ee6ab2f271b250353bd20c7f134771284b
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-06-06 21:19:40 -0600

    Update test_main_game.py

--- test_main_game.py
+++ test_main_game.py
@@ -3,11 +3,10 @@ import battle
 
 
 def main():
-
     while True:
         choice = input('a or s: ')
         if choice == 's':
-            potions = potion.start_potion_game()
+            potion.start_potion_game()
         elif choice == 'a':
             battle.start_battle()
 main()
