03b29220e8fc5c601dd169deaef3a9499ba91ab9
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-06-06 23:23:45 -0600

    Update character.py

--- character.py
+++ character.py
@@ -27,16 +27,14 @@ class Character:
 
 class Player(Character):
     
-    def __init__(self, name, increase_attack, increase_damage, potion.PotionGame()potion_inventory):
+    def __init__(self, name, increase_attack, increase_damage, timer_on):
         Character.__init__(self, name)
         self.wand_type = wand.Wand.get_wand()
         self.players_score = 0
         self.increase_attack = increase_attack
         self.increase_damage = increase_damage
-        if potion.PotionGame()potion_inventory:
-            self.potion_inventory = potion.PotionGame()potion_inventory
-        else:
-            self.potion_inventory = []
+        self.timer_on = timer_on
+        self.potions = potion.PotionGame().potion_inventory
         
 
     def movement(self):
@@ -45,13 +43,13 @@ class Player(Character):
 
     def inventory(self): # change to choose_potion
         i = 1
-        for item in self.potion_inventory:
+        for item in self.potions:
             print(f"{i}. {item}")
             i += 1
         try:
             choice = int(input("What potion do you want to use?: "))
-            if 1 <= choice <= len(self.potion_inventory):
-                selected = self.potion_inventory.pop(choice-1)
+            if 1 <= choice <= len(self.potions):
+                selected = self.potions.pop(choice-1)
                 if selected == 'Wiggenweld Potion':
                     print(f'\nYou have used the Wiggenweld Potion!')
                     self.healing()
@@ -59,7 +57,7 @@ class Player(Character):
                     print(f'\nYou have used the Maxima Potion!')
                     print(f'\nYour attacks will increase by 5 damage for the next 30 seconds!')
                     self.increase_attack = True
-                    #self.start_countdown(15)
+                    self.start_countdown(15)
                 elif selected == 'Thunderbrew Potion':
                     print(f'\nYou have used the Thunderbrew Potion')
                     print(f'\nYour next attack will be increased by 50 damage!')
@@ -74,9 +72,12 @@ class Player(Character):
         self.health += 20
         print('drinking...')
         time.sleep(1)
+        print('+ 10 health!')
         print('drinking...')
         time.sleep(1)
-        print('+ 20 health!')
+        print('+ 10 health!')
+        time.sleep(0.5)
+        print("You have gain 20 HP!")
 
 
     def start_countdown(self, duration):
@@ -92,8 +93,12 @@ class Player(Character):
             print(f"\r[Maxima Boost Time left: {timer}]  ", end=" ", flush=True) 
             time.sleep(1) 
             t -= 1
-        self.increase_attack = False
         print("Time's up!")
+        if self.increase_attack:
+            self.increase_attack = False
+        if self.timer_on: 
+            self.timer_on = False
+
 
 
     def inflict_damage(self, enemy):
@@ -108,16 +113,26 @@ class Player(Character):
             attack += 50
             print("BAMMMM THUNDERSTRIKE")
             self.increase_damage = False
-        if self.health <= 60:
+        if self.health <= 100:
             print('Blimey, yeh not lookin too good on health! Quick! Think back to class — cast any one of the spell yeh remember to cause more damage!')
-            
+            i = 1
+            for item in spells.Spell.spell_list:
+                print(f"{i}. {item}")
+                i += 1
+            self.timer_on = True
+            self.start_countdown(5)
             choice = input("spell name: ").title()
-            spells.Spell(choice).cast_spell()
-            spell_attack = spells.Spell(choice).attack  
-            total_attack = attack + spell_attack
-            print(f"{enemy.name} has taken a total of {total_attack} damage")
-            enemy.take_damage(total_attack)
-            return total_attack
+            if not self.timer_on and choice == None:
+                print("You did not cast any spells in time")
+                total_attack = 0
+                return total_attack
+            else:
+                spells.Spell(choice).cast_spell()
+                spell_attack = spells.Spell(choice).attack  
+                total_attack = attack + spell_attack
+                print(f"{enemy.name} has taken a total of {total_attack} damage")
+                enemy.take_damage(total_attack)
+                return total_attack
         else:
             print(f"{enemy.name} has taken {attack} damage")
             enemy.take_damage(attack)
@@ -149,10 +164,6 @@ class Oponent(Character):
         user.take_damage(attack)
         return attack
                
-
-
-
-
 
 
 
