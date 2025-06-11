0fb7c8b6975bc706c8a59fd7d6a1f390c4c96247
EmilyT2011 <khanh.trinh1@rbe.sk.ca>
2025-06-11 00:14:02 -0600

    Update character.py

--- character.py
+++ character.py
@@ -39,7 +39,7 @@ class Player(Character):
         
     def movement(self):
         pass
-        #movement.player.main_menu()
+        #movement.player.main_menu()       
             
     def healing(self):
         self.health += 20
